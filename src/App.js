2248c4eae5f46004c195924baf2ceb3b3671f138
cantuario2 <cantuario2@gmail.com>
2022-06-13 19:01:46 -0300

    20220613_housekeeping

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import '@splidejs/splide/dist/css/themes/splide-skyblue.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Splide>
+        <SplideSlide>
+          <img src={logo} alt="Imagem 1"/>
+        </SplideSlide>
+        <SplideSlide>
+          <img src={logo} alt="Imagem 2"/>
+        </SplideSlide>
+      </Splide>
+    </>
   );
 }
 
